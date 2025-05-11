56f0c9146813055299999dabd329feea1f4d557c
Igonorant <igolino3@hotmail.com>
2025-05-11 16:53:47 -0300

    Added zombie behavior to follow mommy

--- Zombie.cs
+++ Zombie.cs
@@ -5,13 +5,22 @@ using System;
 
 public partial class Zombie : Character
 {
-    [Export] public CharacterBody2D target;
+    [Export] public CharacterBody2D mommy;
+    [Export] public CharacterBody2D baby;
     [Export] public float speed = 50.0f;
 
     public override void _PhysicsProcess(double delta)
     {
-        Vector2 direction = target.GlobalPosition - GlobalPosition;
-        Velocity = direction.Normalized() * speed;
+        Vector2 distanceToMommy = mommy.GlobalPosition - GlobalPosition;
+        if (distanceToMommy.Length() < 75.0f)
+        {
+            Velocity = distanceToMommy.Normalized() * speed;
+        }
+        else
+        {
+            Vector2 distanceToBaby = baby.GlobalPosition - GlobalPosition;
+            Velocity = distanceToBaby.Normalized() * speed;
+        }
         MoveAndSlide();
     }
 }

16a4af6a522de02d7ab06d92a35a06f6cb94376a
Igonorant <igolino3@hotmail.com>
2025-05-12 15:47:51 -0300

    Added baby brain

--- Baby.cs
+++ Baby.cs
@@ -3,204 +3,28 @@ using System;
 
 public partial class Baby : Character
 {
-    [Export] public bool enableDebug = false;
-    [Export] public CharacterBody2D mommy;
+
+    [Export] public Character mommy;
     [Export] public float maxDistanceToMommy = 200.0f;
     [Export] public float babySpeed = 50.0f;
     [Export] public float wanderingTime = 2.0f;
     [Export] public float idleTime = 1.0f;
     [Export] public float followingTime = 2.0f;
 
-    private RandomNumberGenerator rng;
 
-    private Vector2 currentDirection = Vector2.Zero;
-
-    private Timer idleTimer;
-    private Timer wanderingTimer;
-    private Timer followingTimer;
-
-    private enum State
-    {
-        IDLE,
-        WANDERING,
-        FOLLOWING,
-        STUNNED
-    }
-    private String GetStateString(State state)
-    {
-        switch (state)
-        {
-            case State.IDLE: return "IDLE";
-            case State.WANDERING: return "WANDERING";
-            case State.FOLLOWING: return "FOLLOWING";
-            case State.STUNNED: return "STUNNED";
-            default: return "Unknown state";
-        }
-    }
-    private State currentState = State.IDLE;
-
-    private AnimationPlayer animation;
-
-    private PackedScene debugLabelScene = (PackedScene)GD.Load("uid://bexfqv8srwl01");
-    private Label debugLabel = null;
-
+    private BabyBrain brain;
 
     public override void _Ready()
     {
-        rng = new RandomNumberGenerator();
-        rng.Randomize();
-
-        idleTimer = (Timer)FindChild("IdleTimer");
-        wanderingTimer = (Timer)FindChild("WanderingTimer");
-        followingTimer = (Timer)FindChild("FollowingTimer");
-        idleTimer.WaitTime = idleTime;
-        wanderingTimer.WaitTime = wanderingTime;
-        followingTimer.WaitTime = followingTime;
-        idleTimer.Timeout += OnIdleTimerTimeout;
-        wanderingTimer.Timeout += OnWanderingTimerTimeout;
-        followingTimer.Timeout += OnFollowingTimerTimeout;
-
-
-        currentState = State.IDLE;
-        idleTimer.Start();
-
-        animation = (AnimationPlayer)FindChild("AnimationPlayer");
-
-        if (enableDebug)
-        {
-            debugLabel = debugLabelScene.Instantiate<Label>();
-            AddChild(debugLabel);
-            debugLabel.Scale = new Vector2(1.0f / Scale.X, 1.0f / Scale.Y);
-            debugLabel.Position = new Vector2(0.0f, -35.0f);
-        }
+        brain = (BabyBrain)FindChild("BabyBrain");
+        brain.SetMommy(mommy, maxDistanceToMommy);
+        brain.SetTimers(idleTime, wanderingTime, followingTime);
     }
 
     public override void _PhysicsProcess(double delta)
     {
-        ComputeNextState();
-
-        switch (currentState)
-        {
-            case State.IDLE: HandleIdleState(); break;
-            case State.WANDERING: HandleWanderingState(); break;
-            case State.FOLLOWING: HandleFollowingState(); break;
-            case State.STUNNED: HandleStunnedState(); break;
-        }
-        SelectAnimation();
+        Velocity = brain.GetMovingDirection() * babySpeed;
+        brain.SelectAnimation();
         MoveAndSlide();
-    }
-
-    private void SelectAnimation()
-    {
-        Vector2 velocityAbs = Velocity.Abs();
-        if (Velocity.IsZeroApprox())
-        {
-            animation.Play("idle");
-        }
-        else
-        {
-            if (velocityAbs.X > velocityAbs.Y)
-            {
-                // Moving right/left
-                if (Velocity.X >= 0.0f)
-                {
-                    animation.Play("moving_right");
-                }
-                else
-                {
-                    animation.Play("moving_left");
-                }
-            }
-            else
-            {
-                if (Velocity.Y <= 0.0f)
-                {
-                    animation.Play("moving_up");
-                }
-                else
-                {
-                    animation.Play("moving_down");
-                }
-            }
-        }
-    }
-
-    private void ComputeNextState()
-    {
-        if (IsTakingDamage())
-        {
-            currentState = State.STUNNED;
-        }
-
-        // Other states are select by the timers' timeout signals
-
-        if (debugLabel != null) { debugLabel.Text = GetStateString(currentState); }
-    }
-
-    private void OnIdleTimerTimeout()
-    {
-        if (IsFarFromMommy())
-        {
-            currentState = State.FOLLOWING;
-            followingTimer.Start();
-        }
-        else
-        {
-            currentState = State.WANDERING;
-            wanderingTimer.Start();
-        }
-
-    }
-
-    private void OnWanderingTimerTimeout()
-    {
-        currentState = State.IDLE;
-        idleTimer.Start();
-    }
-
-    private void OnFollowingTimerTimeout()
-    {
-        currentState = State.IDLE;
-        idleTimer.Start();
-    }
-
-    private bool IsTakingDamage()
-    {
-        // TODO
-        return false;
-    }
-
-    private bool IsFarFromMommy()
-    {
-        return (mommy.GlobalPosition - GlobalPosition).Length() > maxDistanceToMommy;
-    }
-
-    private void HandleIdleState()
-    {
-        Velocity = Vector2.Zero;
-    }
-
-    private void HandleWanderingState()
-    {
-        if (currentDirection.IsZeroApprox())
-        {
-            currentDirection.X = rng.RandfRange(-1.0f, 1.0f);
-            currentDirection.Y = rng.RandfRange(-1.0f, 1.0f);
-            currentDirection = currentDirection.Normalized();
-        }
-        Velocity = currentDirection * babySpeed;
-    }
-
-    private void HandleFollowingState()
-    {
-        currentDirection = Vector2.Zero;
-        Vector2 distance_to_mommy = mommy.GlobalPosition - GlobalPosition;
-        Velocity = distance_to_mommy.Normalized() * babySpeed;
-    }
-
-    private void HandleStunnedState()
-    {
-        // TODO
-        Velocity = Vector2.Zero;
     }
 }
---
+++ BabyBrain.cs
@@ -0,0 +1,211 @@
+using Godot;
+using System;
+
+public partial class BabyBrain : BrainComponent
+{
+    [Export] public bool enableDebug = false;
+
+    private RandomNumberGenerator rng;
+    private Vector2 currentDirection = Vector2.Zero;
+    private Character mommy;
+    private float maxDistanceToMommy;
+
+    private Timer idleTimer;
+    private Timer wanderingTimer;
+    private Timer followingTimer;
+    private enum State
+    {
+        IDLE,
+        WANDERING,
+        FOLLOWING,
+        STUNNED
+    }
+    private String GetStateString(State state)
+    {
+        switch (state)
+        {
+            case State.IDLE: return "IDLE";
+            case State.WANDERING: return "WANDERING";
+            case State.FOLLOWING: return "FOLLOWING";
+            case State.STUNNED: return "STUNNED";
+            default: return "Unknown state";
+        }
+    }
+    private State currentState = State.IDLE;
+
+    [Export] private AnimationPlayer animation;
+
+
+    private PackedScene debugLabelScene = (PackedScene)GD.Load("uid://bexfqv8srwl01");
+    private Label debugLabel = null;
+
+
+    public override void _Ready()
+    {
+        base._Ready();
+        rng = new RandomNumberGenerator();
+        rng.Randomize();
+
+        idleTimer = (Timer)FindChild("IdleTimer");
+        wanderingTimer = (Timer)FindChild("WanderingTimer");
+        followingTimer = (Timer)FindChild("FollowingTimer");
+        idleTimer.Timeout += OnIdleTimerTimeout;
+        wanderingTimer.Timeout += OnWanderingTimerTimeout;
+        followingTimer.Timeout += OnFollowingTimerTimeout;
+
+        currentState = State.IDLE;
+        idleTimer.Start();
+
+        if (enableDebug)
+        {
+            debugLabel = debugLabelScene.Instantiate<Label>();
+            debugLabel.Scale = new Vector2(1.0f / parentCharacter.Scale.X, 1.0f / parentCharacter.Scale.Y);
+            debugLabel.Position = new Vector2(0.0f, -35.0f);
+            parentCharacter.CallDeferred("add_child", debugLabel);
+        }
+    }
+
+    public void SetTimers(float idleTime, float wanderingTime, float followingTime)
+    {
+        idleTimer.WaitTime = idleTime;
+        wanderingTimer.WaitTime = wanderingTime;
+        followingTimer.WaitTime = followingTime;
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        ComputeNextState();
+
+        switch (currentState)
+        {
+            case State.IDLE: HandleIdleState(); break;
+            case State.WANDERING: HandleWanderingState(); break;
+            case State.FOLLOWING: HandleFollowingState(); break;
+            case State.STUNNED: HandleStunnedState(); break;
+        }
+    }
+
+    public void SelectAnimation()
+    {
+        Vector2 velocityAbs = parentCharacter.Velocity.Abs();
+        if (parentCharacter.Velocity.IsZeroApprox())
+        {
+            animation.Play("idle");
+        }
+        else
+        {
+            if (velocityAbs.X > velocityAbs.Y)
+            {
+                // Moving right/left
+                if (parentCharacter.Velocity.X >= 0.0f)
+                {
+                    animation.Play("moving_right");
+                }
+                else
+                {
+                    animation.Play("moving_left");
+                }
+            }
+            else
+            {
+                if (parentCharacter.Velocity.Y <= 0.0f)
+                {
+                    animation.Play("moving_up");
+                }
+                else
+                {
+                    animation.Play("moving_down");
+                }
+            }
+        }
+    }
+
+    private void ComputeNextState()
+    {
+        if (IsTakingDamage())
+        {
+            currentState = State.STUNNED;
+        }
+
+        // Other states are select by the timers' timeout signals
+
+        if (debugLabel != null) { debugLabel.Text = GetStateString(currentState); }
+    }
+
+    private void OnIdleTimerTimeout()
+    {
+        if (IsFarFromMommy())
+        {
+            currentState = State.FOLLOWING;
+            followingTimer.Start();
+        }
+        else
+        {
+            currentState = State.WANDERING;
+            wanderingTimer.Start();
+        }
+
+    }
+
+    private void OnWanderingTimerTimeout()
+    {
+        currentState = State.IDLE;
+        idleTimer.Start();
+    }
+
+    private void OnFollowingTimerTimeout()
+    {
+        currentState = State.IDLE;
+        idleTimer.Start();
+    }
+
+    private bool IsTakingDamage()
+    {
+        // TODO
+        return false;
+    }
+
+    private bool IsFarFromMommy()
+    {
+        return (mommy.GlobalPosition - parentCharacter.GlobalPosition).Length() > maxDistanceToMommy;
+    }
+
+    private void HandleIdleState()
+    {
+        currentDirection = Vector2.Zero;
+    }
+
+    private void HandleWanderingState()
+    {
+        if (currentDirection.IsZeroApprox())
+        {
+            currentDirection.X = rng.RandfRange(-1.0f, 1.0f);
+            currentDirection.Y = rng.RandfRange(-1.0f, 1.0f);
+            currentDirection = currentDirection.Normalized();
+        }
+    }
+
+    private void HandleFollowingState()
+    {
+        currentDirection = Vector2.Zero;
+        currentDirection = (mommy.GlobalPosition - parentCharacter.GlobalPosition).Normalized();
+    }
+
+    private void HandleStunnedState()
+    {
+        // TODO
+        currentDirection = Vector2.Zero;
+    }
+
+
+    public void SetMommy(Character character, float maxDistance)
+    {
+        mommy = character;
+        maxDistanceToMommy = maxDistance;
+    }
+
+    public override Vector2 GetMovingDirection()
+    {
+        return currentDirection;
+    }
+}

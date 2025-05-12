677b92581880d81a57fe90cc4b6f2a3bdf105be7
Igonorant <igolino3@hotmail.com>
2025-05-11 21:35:41 -0300

    Started to add zombie brain

--- Zombie.cs
+++ Zombie.cs
@@ -7,123 +7,38 @@ using System.Reflection.Metadata;
 
 public partial class Zombie : Character
 {
-    [Export] public bool enableDebug = false;
-    [Export] public CharacterBody2D mommy;
-    [Export] public CharacterBody2D baby;
+    [Export] public Character mommy;
+    [Export] public Character baby;
     [Export] public float speed = 50.0f;
     [Export] public float chasingMommyDistance = 75.0f;
-    [Export] public float attackingRange = 50.0f;
+    [Export] public float attackRange = 50.0f;
 
-    private enum State
-    {
-        IDLE,
-        CHASING,
-        ATTACKING,
-        STUNNED
-    }
-    private String GetStateString(State state)
-    {
-        switch (state)
-        {
-            case State.IDLE: return "IDLE";
-            case State.CHASING: return "CHASING";
-            case State.ATTACKING: return "ATTACKING";
-            case State.STUNNED: return "STUNNED";
-            default: return "Unknown state";
-        }
-    }
 
-    private State currentState = State.IDLE;
-
-    private CharacterBody2D target;
-
-    private PackedScene debugLabelScene = (PackedScene)GD.Load("uid://bexfqv8srwl01");
-    private Label debugLabel = null;
+    private ZombieBrain brain;
 
     public override void _Ready()
     {
-        target = baby;
-        if (enableDebug)
-        {
-            debugLabel = debugLabelScene.Instantiate<Label>();
-            AddChild(debugLabel);
-            debugLabel.Scale = new Vector2(1.0f / Scale.X, 1.0f / Scale.Y);
-        }
+        base._Ready();
+        // attackComponent = (AttackComponent)FindChild("AttackComponent");
+        brain = (ZombieBrain)FindChild("ZombieBrain");
+        brain.SetTarget(baby);
+        brain.SetAttackRange(attackRange);
     }
-
 
     public override void _PhysicsProcess(double delta)
     {
-        ComputeNextState();
-        if (debugLabel != null) { debugLabel.Text = GetStateString(currentState) + "\n" + target.Name; }
+        Vector2 distanceToMommy = mommy.GlobalPosition - GlobalPosition;
+        brain.SetTarget(distanceToMommy.Length() < chasingMommyDistance ? mommy : baby);
 
-        switch (currentState)
-        {
-            case State.IDLE: HandleIdleState(); break;
-            case State.CHASING: HandleChasingState(); break;
-            case State.ATTACKING: HandleAttackingState(); break;
-            case State.STUNNED: HandleStunnedState(); break;
-        }
+        Vector2 direction = brain.GetMovingDirection();
+        Velocity = direction * 200;
+
+        // if (!Velocity.IsZeroApprox())
+        // {
+        //     attackComponent.Rotation = brain.GetAttackDirection().Angle();
+        // }
 
         MoveAndSlide();
     }
 
-    private void ComputeNextState()
-    {
-        if (IsTakingDamage())
-        {
-            currentState = State.STUNNED;
-            return;
-        }
-
-        if (TargetInAttackRange())
-        {
-            currentState = State.ATTACKING;
-            return;
-        }
-
-        currentState = State.CHASING;
-        Vector2 distanceToMommy = mommy.GlobalPosition - GlobalPosition;
-        if (distanceToMommy.Length() < chasingMommyDistance)
-        {
-            target = mommy;
-        }
-        else
-        {
-            target = baby;
-        }
-    }
-
-    private bool TargetInAttackRange()
-    {
-        return (target.GlobalPosition - GlobalPosition).Length() < attackingRange;
-    }
-
-    private bool IsTakingDamage()
-    {
-        // TODO
-        return false;
-    }
-
-    private void HandleIdleState()
-    {
-        Velocity = Vector2.Zero;
-    }
-
-    private void HandleChasingState()
-    {
-        Velocity = (target.GlobalPosition - GlobalPosition).Normalized() * speed;
-    }
-
-    private void HandleAttackingState()
-    {
-        // TODO
-        Velocity = Vector2.Zero;
-    }
-
-    private void HandleStunnedState()
-    {
-        // TODO
-        Velocity = Vector2.Zero;
-    }
 }
---
+++ ZombieBrain.cs
@@ -0,0 +1,134 @@
+using Godot;
+using System;
+
+public partial class ZombieBrain : BrainComponent
+{
+    // Debug variables
+    [Export] public bool enableDebug = false;
+    private PackedScene debugLabelScene = (PackedScene)GD.Load("uid://bexfqv8srwl01");
+    private Label debugLabel = null;
+
+    // States
+    private enum State
+    {
+        IDLE,
+        CHASING,
+        ATTACKING,
+        STUNNED
+    }
+    private String GetStateString(State state)
+    {
+        switch (state)
+        {
+            case State.IDLE: return "IDLE";
+            case State.CHASING: return "CHASING";
+            case State.ATTACKING: return "ATTACKING";
+            case State.STUNNED: return "STUNNED";
+            default: return "Unknown state";
+        }
+    }
+    private State currentState = State.IDLE;
+
+
+
+    private Vector2 last_direction = Vector2.Zero;
+    private Character target;
+    private float attackRange;
+
+
+
+    public override void _Ready()
+    {
+        base._Ready();
+        if (enableDebug)
+        {
+            debugLabel = debugLabelScene.Instantiate<Label>();
+            parentCharacter.CallDeferred("AddChild", debugLabel);
+            debugLabel.Scale = new Vector2(1.0f / parentCharacter.Scale.X, 1.0f / parentCharacter.Scale.Y);
+        }
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        ComputeNextState();
+        if (debugLabel != null) { debugLabel.Text = GetStateString(currentState) + "\n" + target.Name; }
+
+        switch (currentState)
+        {
+            case State.IDLE: HandleIdleState(); break;
+            case State.CHASING: HandleChasingState(); break;
+            case State.ATTACKING: HandleAttackingState(); break;
+            case State.STUNNED: HandleStunnedState(); break;
+        }
+
+    }
+
+    private void ComputeNextState()
+    {
+        if (IsTakingDamage())
+        {
+            currentState = State.STUNNED;
+            return;
+        }
+
+        if (TargetInAttackRange())
+        {
+            currentState = State.ATTACKING;
+            return;
+        }
+
+        currentState = State.CHASING;
+    }
+
+    private bool TargetInAttackRange()
+    {
+        return (target.GlobalPosition - parentCharacter.GlobalPosition).Length() < attackRange;
+    }
+
+    private bool IsTakingDamage()
+    {
+        // TODO
+        return false;
+    }
+
+    private void HandleIdleState()
+    {
+        last_direction = Vector2.Zero;
+    }
+
+    private void HandleChasingState()
+    {
+        last_direction = (target.GlobalPosition - parentCharacter.GlobalPosition).Normalized();
+    }
+
+    private void HandleAttackingState()
+    {
+        // TODO
+        last_direction = Vector2.Zero;
+    }
+
+    private void HandleStunnedState()
+    {
+        // TODO
+        last_direction = Vector2.Zero;
+    }
+
+    public override Vector2 GetMovingDirection()
+    {
+
+        return last_direction;
+    }
+
+    public override Vector2 GetAttackDirection() { return last_direction; }
+
+    public void SetTarget(Character newTarget)
+    {
+        target = newTarget;
+    }
+
+    public void SetAttackRange(float range)
+    {
+        attackRange = range;
+    }
+
+}
